6eea010993f5ba5490baf667afdd0d6b234fbe6f
smsohan <sohan39@gmail.com>
2014-12-29 16:53:26 -0700

    symlinks to secrets

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,20 +3,25 @@ lock '3.3.5'
 set :application, 'api_through_ui'
 set :repo_url, 'git@github.com:smsohan/api_through_ui.git'
 
+set :linked_dirs, fetch(:linked_dirs, []).push('secrets')
+
 SSHKit.config.command_map[:build_and_run] = "#{current_path}/build_and_run.sh"
 
 namespace :deploy do
 
   task :build_and_run do
     on roles(:app) do
+
       within current_path do
-        with SECRET_KEY_BASE: ENV['SECRET_KEY_BASE'] do
+        secret_key_base = execute('cat', 'secrets/SECRET_KEY_BASE').strip
+
+        with SECRET_KEY_BASE: secret_key_base do
           execute :build_and_run
         end
       end
     end
   end
 
-  after :finished, :build_and_run
+  after :finished, [:build_and_run]
 
 end
