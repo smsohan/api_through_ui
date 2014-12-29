11ea868f327628e3d403b12761c327023b111512
smsohan <sohan39@gmail.com>
2014-12-29 16:10:53 -0700

    secret

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ set +e
 docker stop api_through_ui
 docker rm api_through_ui
 set -e
-docker run -p 80:80 --name api_through_ui -d --link mongodb:mongodb smsohan/api_through_ui
+docker run -p 80:80 --name api_through_ui -d --link mongodb:mongodb --env SECRET_KEY_BASE=$SECRET_KEY_BASE smsohan/api_through_ui
