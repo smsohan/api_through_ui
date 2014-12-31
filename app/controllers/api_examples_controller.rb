8c7d71fb8d167f7a4ee2c396c841c0fc91326119
smsohan <sohan39@gmail.com>
2014-12-31 15:52:23 -0700

    shows the index action

--- app/controllers/api_examples_controller.rb
+++ app/controllers/api_examples_controller.rb
@@ -1,11 +1,11 @@
 class ApiExamplesController < ApplicationController
 
-  # def index
-  #   @api_host = ApiHost.new(name: params[:api_host])
-  #   @api_version = ApiVersion.new(name: params[:api_version], api_host: @api_host)
-  #   @api_resource = ApiResource.new(name: params[:api_resource], api_version: @api_version)
-  #   @api_examples = @api_resource.api_examples
-  # end
+  def index
+    @api_host = ApiHost.new(name: params[:api_host])
+    @api_version = ApiVersion.new(name: params[:api_version], api_host: @api_host)
+    @api_resource = ApiResource.new(name: params[:api_resource], api_version: @api_version)
+    @api_examples = @api_resource.api_examples
+  end
 
   def show
     @index = params[:id].to_i
