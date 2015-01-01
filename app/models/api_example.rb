8bdb52bd8bbedf8d40726745ee3d58c74b07c980
smsohan <sohan39@gmail.com>
2015-01-01 12:27:04 -0700

    recorded at added

--- app/models/api_example.rb
+++ app/models/api_example.rb
@@ -5,16 +5,29 @@ class ApiExample
   field :url, type: String
   field :http_method, type: String
   field :version, type: String
+  field :resource, type: String
   field :description, type: String
   field :query, type: Hash, default: {}
   field :requestHeaders, type: Hash, as: :request_headers
   field :requestBody, type: String, as: :request_body
   field :responseHeaders, type: Hash, as: :response_headers
   field :responseBody, type: String, as: :response_body
+  field :recordedAt, type: DateTime, as: :recorded_at
 
   def prettified_response
     JSON.pretty_generate(JSON.parse(response_body))
   rescue
     response_body
+  end
+
+  def to_curl
+    curl_headers = request_headers.merge( 'x-spy-rest-desc' => description,
+      'x-spy-rest-resource' => resource,
+      'x-spy-rest-version' => version
+       )
+
+    curl_headers_string = curl_headers.map{|key, value| "-H '#{key}: #{value}'"}.join(' ')
+
+    "curl -k -x 'http://spyrest.com:9081' #{curl_headers_string} 'https://api.github.com#{url}'"
   end
 end
