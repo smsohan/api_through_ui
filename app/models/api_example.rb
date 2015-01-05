b26a278f377e289671d9a8a72c75ed548290d1d0
smsohan <sohan39@gmail.com>
2015-01-05 13:46:17 -0700

    strings

--- app/models/api_example.rb
+++ app/models/api_example.rb
@@ -13,7 +13,7 @@ class ApiExample
   field :requestBody, type: String, as: :request_body
   field :responseHeaders, type: Hash, as: :response_headers
   field :responseBody, type: String, as: :response_body
-  field :strippedResponseBody, type: Hash, as: :stripped_response_body
+  field :strippedResponseBody, type: String, as: :stripped_response_body
   field :recordedAt, type: DateTime, as: :recorded_at
 
   def to_curl
