0e49e0fa05d3107d10f4c7c3a3e72eb9c8bf5369
smsohan <sohan39@gmail.com>
2015-01-14 22:33:33 -0700

    breadcrumbs fixed

--- app/controllers/api_actions_controller.rb
+++ app/controllers/api_actions_controller.rb
@@ -7,7 +7,7 @@ class ApiActionsController < ApplicationController
 
     add_breadcrumb @api_host.name + " Versions", api_versions_path(api_host: @api_host.name)
     add_breadcrumb @api_version.name + ' Resources', api_resources_path(api_host: @api_host.name, api_version: @api_version.name)
-    add_breadcrumb @api_resource.name, api_resources_path(api_host: @api_host.name, api_version: @api_version.name, api_resource: @api_resource.name)
+    add_breadcrumb @api_resource.name, api_resource_path(@api_resource.name, api_host: @api_host.name, api_version: @api_version.name)
     add_breadcrumb @api_action.name
   end
 
