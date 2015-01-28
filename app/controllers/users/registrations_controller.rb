fb6f1b9ac275f087c8fba3ebd4caaa0c69976d65
mo khan <mo@mokhan.ca>
2015-01-27 21:23:40 -0700

    allow editing username from edit page.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_filter :configure_sign_up_params, only: [:create]
-  # before_filter :configure_account_update_params, only: [:update]
+  before_filter :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -44,9 +44,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   # You can put the params you want to permit in the empty array.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.for(:account_update) << :attribute
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.for(:account_update) << :username
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
