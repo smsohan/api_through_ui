a7164143e4f58bc28d44853ce85ce77b73330015
smsohan <sohan39@gmail.com>
2015-09-28 16:13:58 -0600

    fixes the restart

--- config/deploy.rb
+++ config/deploy.rb
@@ -87,7 +87,7 @@ namespace :deploy do
 
         as :root do
           with path: "/bin:/usr/bin:/usr/local/bin" do
-            execute :svc, '-t /service/api_through'
+            execute :svc, '-t /service/api_through_ui'
           end
         end
       end
